d17cacfefeedc29fb8c2ecffa1fe63e565d78525
Airat Akhmetshin <st1ncnpk@gmail.com>
2019-06-05 00:11:01 +0300

    Добавил отрисовку таблицы с номерами

--- src/Controller/PhonebookController.php
+++ src/Controller/PhonebookController.php
@@ -2,18 +2,56 @@
 
 namespace App\Controller;
 
+use App\Entity\Subdivision;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PhonebookController extends AbstractController
 {
     /**
-     * @Route("/phonebook", name="phonebook")
+     * @Route("/", name="homepage")
+     * @return Response
      */
-    public function index()
+    public function index(): Response
     {
+        $subdivisions = $this->getDoctrine()
+            ->getRepository(Subdivision::class)
+            ->findBy([], ['priority' => 'DESC']);
+
         return $this->render('phonebook/index.html.twig', [
-            'controller_name' => 'PhonebookController',
+            'subdivisions' => $subdivisions
+        ]);
+    }
+
+    /**
+     * @Route("/list/all", name="list")
+     * @return Response
+     */
+    public function list(): Response
+    {
+        $subdivisions = $this->getDoctrine()
+            ->getRepository(Subdivision::class)
+            ->findBy([], ['priority' => 'DESC']);
+
+        return $this->render('phonebook/list.html.twig', [
+            'subdivisions' => $subdivisions
+        ]);
+    }
+
+    /**
+     * @Route("/list/{id}", name="listBy", requirements={"page"="\d+"})
+     * @param int $id
+     * @return Response
+     */
+    public function listBy(int $id): Response
+    {
+        $subdivision = $this->getDoctrine()
+            ->getRepository(Subdivision::class)
+            ->find($id);
+
+        return $this->render('phonebook/list.html.twig', [
+            'subdivisions' => [$subdivision],
         ]);
     }
 }
