752fba4d71d547c9041b4dc01adfabb8ac519f91
Airat Akhmetshin <st1ncnpk@gmail.com>
2019-06-19 23:41:38 +0300

    Добавил форматирование для 6- и 1-значных номеров

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -14,7 +14,7 @@ class AppExtension extends AbstractExtension
             // If your filter generates SAFE HTML, you should add a third
             // parameter: ['is_safe' => ['html']]
             // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
-            new TwigFilter('filter_name', [$this, 'doSomething']),
+            new TwigFilter('phoneNumber', [$this, 'formatPhoneNumber']),
         ];
     }
 
@@ -28,5 +28,22 @@ class AppExtension extends AbstractExtension
     public function doSomething($value)
     {
         // ...
+    }
+
+    public function formatPhoneNumber($number)
+    {
+        if (strlen($number) === 6) {
+            return implode('-', str_split($number, 2));
+        }
+
+        if (strlen($number) === 11) {
+            $countryCode                   = substr($number, 0, 1);
+            [$areaCode, $phoneNumberA]     = str_split(substr($number, 1, 6), 3);
+            [$phoneNumberB, $phoneNumberC] = str_split(substr($number, 7, 4), 2);
+
+            return "$countryCode ($areaCode) $phoneNumberA-$phoneNumberB-$phoneNumberC";
+        }
+
+        return $number;
     }
 }
