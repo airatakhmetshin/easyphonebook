6813df2602e2021e78ff0e5e954e45818386a3d2
Airat Akhmetshin <st1ncnpk@gmail.com>
2020-05-16 22:27:01 +0300

    Update dependencies

--- src/Repository/SubdivisionRepository.php
+++ src/Repository/SubdivisionRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Subdivision;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Subdivision|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class SubdivisionRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Subdivision::class);
     }
